fa62632409b3d4a5a3b5c18c769bbf501edb8132
DollyTheeSheep <ddkandogan@gmail.com>
2022-02-09 13:43:51 +0300

    Kumanda icin tanimlama ve okuma yapildi. Denenmedi !

--- Simulation_Control.py
+++ Simulation_Control.py
@@ -1,5 +1,22 @@
 import threading
+import pygame
 
+class controller():
+    controller = None
+    axis_data = None
+    def controller_init(self):
+        pygame.init()
+        pygame.joystick.init()
+        self.controller=pygame.joystick.Joystick(0)
+        self.controller.init()
+        if not self.axis_data:
+            self.axis_data = {}
+    def listen(self):
+        for event in pygame.event.get():
+            if event.type == pygame.JOYAXISMOTION:
+                self.axis_data[event.axis] = round(event.value, 2)
+                for i in len(self.axis_data):
+                    print(self.axis_data[i])
 
 class HZ5(threading.Thread):
     def __init__(self, event):
@@ -8,6 +25,8 @@ class HZ5(threading.Thread):
 
     def run(self):
         while not self.stopped.wait(0.2):
+            #/------------------------------------------------------- 5 Hz ile yapilmasi gereken islemleri buranin altina yaz -------------------------------------------------------\#
+
             print("5 HZ")
 
 class HZ10(threading.Thread):
@@ -17,6 +36,8 @@ class HZ10(threading.Thread):
 
     def run(self):
         while not self.stopped.wait(0.1):
+            #/------------------------------------------------------- 10 Hz ile yapilmasi gereken islemleri buranin altina yaz -------------------------------------------------------\#
+            kumanda.listen()
             print("10 HZ")
 
 if __name__ == '__main__':
@@ -24,7 +45,11 @@ if __name__ == '__main__':
 
     thread_5HZ = HZ5(stopFlag)
     thread_10HZ = HZ10(stopFlag)
+    #/------------------------------------------------------- Kumandayı Oluştur -------------------------------------------------------\#
+    kumanda = controller()
+    kumanda.controller_init()
 
-    # /------------------------------------------------------- Sayaçları Başlat -------------------------------------------------------\#
+    #/------------------------------------------------------- Sayaçları Başlat -------------------------------------------------------\#
     #thread_5HZ.start()
     #thread_10HZ.start()
+
