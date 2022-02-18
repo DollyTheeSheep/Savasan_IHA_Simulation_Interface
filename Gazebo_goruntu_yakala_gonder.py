ccf73ad7026d012d30c02c1184c405e2fcb25340
DollyTheeSheep <ddkandogan@gmail.com>
2022-02-18 15:30:21 +0300

    Sim_control kumanda ile birlesti. Gazebodan goruntu alma ve UDP ile gonderme yapildi. UDP'den goruntu alma eklendi.

---
+++ Gazebo_goruntu_yakala_gonder.py
@@ -0,0 +1,81 @@
+import rospy
+import cv2, imutils, socket
+import numpy as np
+import time
+import base64
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class goruntu:
+    def __init__(self):
+        self.BUFF_SIZE = 65536
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, self.BUFF_SIZE)
+        self.host_name = socket.gethostname()
+        self.host_ip = '127.0.0.1'  # socket.gethostbyname(host_name)
+        print(self.host_ip)
+        self.port = 9999
+        self.socket_address = (self.host_ip, self.port)
+        self.server_socket.bind(self.socket_address)
+        self.WIDTH = 400
+        self.fps, self.st, self.frames_to_count, self.cnt = (0, 0, 20, 0)
+        self.msg, self.client_addr = self.server_socket.recvfrom(self.BUFF_SIZE)
+        print('GOT connection from ', self.client_addr)
+    def goruntu_aktar(self, data):
+        self.frame = imutils.resize(data, width=self.WIDTH)
+        self.encoded, self.buffer = cv2.imencode('.jpg', self.frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
+        self.message = base64.b64encode(self.buffer)
+        self.server_socket.sendto(self.message, self.client_addr)
+        self.frame = cv2.putText(self.frame, 'FPS: ' + str(self.fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.imshow('TRANSMITTING VIDEO', self.frame)
+        self.key = cv2.waitKey(1) & 0xFF
+        if self.key == ord('q'):
+            self.server_socket.close()
+        if self.cnt == self.frames_to_count:
+            try:
+                self.fps = round(self.frames_to_count / (time.time() - self.st))
+                self.st = time.time()
+                self.cnt = 0
+            except:
+                pass
+        self.cnt += 1
+class camera_1:
+    def __init__(self):
+        #/--------------------------------------- ROS ---------------------------------------\#
+        self.image_sub = rospy.Subscriber("/ZephyrPlane/boreas/camera/image_raw", Image, self.callback)
+        #/--------------------------------------- Socket ---------------------------------------\#
+        self.a=0
+
+
+    def callback(self, data):
+        #/--------------------------------------- Ros ---------------------------------------\#
+        bridge = CvBridge()
+        try:
+            cv_image = bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
+        except CvBridgeError as e:
+            rospy.logerr(e)
+
+        image = cv_image
+        frame = cv2.resize(image, (400, 400))
+        #cv2.imshow("Camera output normal", self.frame)
+        # /--------------------------------------- Socket ---------------------------------------\#
+        if self.a == 0:
+            self.video = goruntu()
+            print("goruntu nesnesi olustu")
+            self.a=1
+        self.video.goruntu_aktar(frame)
+
+
+def main():
+    camera_1()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        rospy.loginfo("Shutting down")
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    rospy.init_node('camera_read', anonymous=False)
+    main()
--- Simulation_Control.py
+++ Simulation_Control.py
@@ -1,23 +1,89 @@
 import threading
+import pygame
+import pygame,time
+from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlobal, Command
+import time
+from pymavlink import mavutil
+class mavlnk():
+    def mavlnk_init(self):
+        self.vehicle = connect("tcp:127.0.0.1:5762", wait_ready=True, baud=57600)
+        print("mavlink baglanti")
+    def mavlnk_kumanda_verilerini_gonder(self,aileron,pitch,throttle,yaw,mode):
+        if mode < 1500 :
+            self.vehicle.mode = "AUTO"
+        else :
+            self.vehicle.mode = "FBWA"
+            self.vehicle.channels.overrides['1'] = aileron
+            self.vehicle.channels.overrides['2'] = pitch
+            self.vehicle.channels.overrides['3'] = throttle
+            self.vehicle.channels.overrides['4'] = yaw
 
+class controller():
+    def controller_init(self):
+        pygame.init()
+        self.joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+        for joy in self.joysticks:
+            joy.init()
+        self.axis0=0    #ROLL
+        self.axis1=0    #PITCH
+        self.axis2=0    #THROTTLE
+        self.axis3=0    #MODE
+        self.axis4=0    #BOS
+        self.axis5=0    #YAW
+
+    def custom_map(self,x,c_min,c_max,t_min,t_max):
+        value = ((x-c_min)*(t_max-t_min))/((c_max-c_min)+t_min)
+        return int(value)
+
+    def kumanda_oku(self):
+        for event in pygame.event.get():
+            if event.type == pygame.JOYAXISMOTION:
+                if (event.axis == 0):
+                    self.axis0=2120-self.custom_map(round(event.value,2),1,-1,0,1100)
+                if (event.axis == 1):
+                    self.axis1=1980-self.custom_map(round(event.value,2),1,-1,0,1100)
+                if (event.axis == 2):
+                    self.axis2=1100+self.custom_map(round(event.value,2),1,-1,0,1100)
+                if (event.axis == 3):
+                    self.axis3=1100+self.custom_map(round(event.value,2),-1,1,0,1100)
+                    if(self.axis3<1000): self.axis3=1000
+                    if(self.axis3 > 2000): self.axis3 = 2050
+                if (event.axis == 5):
+                    self.axis5 = 2100-self.custom_map(round(event.value, 2), 1, -1, 0, 1100)
+            if event.type == pygame.JOYBUTTONDOWN:
+                print(event.button)
+
+        print("Axis0-ROLL:",self.axis0,"Axis1-PITCH:",self.axis1,"Axis2-THROTTLE:",self.axis2,"Axis3-MODE:",self.axis3,"Axis4:",self.axis4,"Axis5-YAW:",self.axis5)
 
 class HZ5(threading.Thread):
     def __init__(self, event):
         threading.Thread.__init__(self)
         self.stopped = event
+        # /------------------------------------------------------- Mavlink Baglantisi Yapildi -------------------------------------------------------\#
+        global baglanti
+        baglanti = mavlnk()
+        baglanti.mavlnk_init()
 
     def run(self):
         while not self.stopped.wait(0.2):
-            print("5 HZ")
-            print("Commit Deneme")
+            #/------------------------------------------------------- 5 Hz Ile Yapilmasi Gereken Islemleri Buranin Altina Yaz -------------------------------------------------------\#
+            baglanti.mavlnk_kumanda_verilerini_gonder(kumanda.axis0,kumanda.axis1,kumanda.axis2,kumanda.axis5,kumanda.axis3)
+            # Normalde 1 tane axis gonderilse 5 hz fakat 4 axis gonderildigi icin mavlink inspector da 20 hz goruluyor.
+            #print("5 Hz")
+
 class HZ10(threading.Thread):
     def __init__(self, event):
         threading.Thread.__init__(self)
         self.stopped = event
-
+        # /------------------------------------------------------- Kumandayı Oluştur -------------------------------------------------------\#
+        global kumanda
+        kumanda = controller()
+        kumanda.controller_init()
     def run(self):
         while not self.stopped.wait(0.1):
-            print("10 HZ")
+            #/------------------------------------------------------- 10 Hz ile yapilmasi gereken islemleri buranin altina yaz -------------------------------------------------------\#
+            kumanda.kumanda_oku()
+
 
 if __name__ == '__main__':
     stopFlag = threading.Event()
@@ -25,6 +91,12 @@ if __name__ == '__main__':
     thread_5HZ = HZ5(stopFlag)
     thread_10HZ = HZ10(stopFlag)
 
-    # /------------------------------------------------------- Sayaçları Başlat -------------------------------------------------------\#
-    #thread_5HZ.start()
-    #thread_10HZ.start()
+    #/------------------------------------------------------- Sayaçları Başlat -------------------------------------------------------\#
+    thread_5HZ.start()
+    thread_10HZ.start()
+
+
+
+
+
+
