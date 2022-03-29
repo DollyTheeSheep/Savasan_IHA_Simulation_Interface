863ec016840d85ec020021b868cf142194eec6ed
DollyTheeSheep <ddkandogan@gmail.com>
2022-03-30 01:14:11 +0300

    Hedef ve kamikaze haritalari icin secenek konuldu

--- interface7.py
+++ interface7.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'INTERFACE5.ui'
+## Form generated from reading UI file 'INTERFACE7.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -17,10 +17,14 @@ from dronekit import connect , Command
 from pymavlink import mavutil
 import numpy as np
 
+
 class Ui_MainWindow(object):
     def __init__(self):
         self.video_size = QSize(640, 480)
+        self.harita = 0
         self.setup_camera()
+        self.hedef_gosterme_hazirlik()
+        self.ihalar = [5, 6, 7, 8, 9, 10, 11, 12, 13]
         self.iha_lat , self.iha_lon = 40.230758, 29.008734
         self.lat = [self.iha_lat, 40.230758, 40.232183, 40.231528, 40.232191, 40.232502, 40.229656, 40.233362,
                     40.229337]
@@ -30,7 +34,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1842, 716)
+        MainWindow.resize(865, 1070)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.comboBox = QComboBox(self.centralwidget)
@@ -39,29 +43,40 @@ class Ui_MainWindow(object):
         self.comboBox.addItem("")
         self.comboBox.addItem("")
         self.comboBox.setObjectName(u"comboBox")
-        self.comboBox.setGeometry(QRect(60, 30, 69, 22))
+        self.comboBox.setGeometry(QRect(730, 630, 69, 22))
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(40, 10, 121, 16))
+        self.label.setGeometry(QRect(730, 600, 121, 16))
         self.KAMIKAZEHAZIRLIK = QPushButton(self.centralwidget)
         self.KAMIKAZEHAZIRLIK.setObjectName(u"KAMIKAZEHAZIRLIK")
-        self.KAMIKAZEHAZIRLIK.setGeometry(QRect(210, 70, 131, 23))
+        self.KAMIKAZEHAZIRLIK.setGeometry(QRect(730, 700, 111, 23))
         self.KAMIKAZEDALIS = QPushButton(self.centralwidget)
         self.KAMIKAZEDALIS.setObjectName(u"KAMIKAZEDALIS")
-        self.KAMIKAZEDALIS.setGeometry(QRect(210, 95, 131, 23))
+        self.KAMIKAZEDALIS.setGeometry(QRect(730, 730, 101, 23))
         self.iha_durum = QLineEdit(self.centralwidget)
         self.iha_durum.setObjectName(u"iha_durum")
-        self.iha_durum.setGeometry(QRect(50, 70, 113, 20))
+        self.iha_durum.setGeometry(QRect(670, 63, 113, 20))
         self.baglanti = QPushButton(self.centralwidget)
         self.baglanti.setObjectName(u"baglanti")
         self.baglanti.setStyleSheet(u"background-color : red")
-        self.baglanti.setGeometry(QRect(330, 20, 101, 23))
+        self.baglanti.setGeometry(QRect(670, 35, 101, 23))
         self.iha_durum_2 = QLineEdit(self.centralwidget)
         self.iha_durum_2.setObjectName(u"iha_durum_2")
-        self.iha_durum_2.setGeometry(QRect(210, 20, 113, 20))
+        self.iha_durum_2.setGeometry(QRect(670, 10, 113, 20))
         self.goruntu = QLabel(self.centralwidget)
         self.goruntu.setObjectName(u"goruntu")
-        self.goruntu.setGeometry(QRect(10, 130, 1800, 550))
+        self.goruntu.setGeometry(QRect(10, 10, 640, 480))
+        self.goruntu_2 = QLabel(self.centralwidget)
+        self.goruntu_2.setObjectName(u"goruntu_2")
+        self.goruntu_2.setGeometry(QRect(10, 500, 700, 550))
+        self.KAMIKAZEDALIS_2 = QPushButton(self.centralwidget)
+        self.KAMIKAZEDALIS_2.setObjectName(u"KAMIKAZEDALIS_2")
+        self.KAMIKAZEDALIS_2.setGeometry(QRect(730, 530, 121, 23))
+        self.comboBox_2 = QComboBox(self.centralwidget)
+        self.comboBox_2.addItem("")
+        self.comboBox_2.addItem("")
+        self.comboBox_2.setObjectName(u"comboBox_2")
+        self.comboBox_2.setGeometry(QRect(730, 500, 121, 23))
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -71,10 +86,12 @@ class Ui_MainWindow(object):
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
-        #BUTONLAR
-        self.baglanti.clicked.connect(self.iha_baglan)
-        self.KAMIKAZEHAZIRLIK.clicked.connect(self.kamikaze_hazirlik)
-        #BUTONLAR
+        # BUTONLAR
+        self.baglanti.clicked.connect(self.iha_baglan_buton)
+        self.KAMIKAZEHAZIRLIK.clicked.connect(self.kamikaze_hazirlik_buton)
+        self.KAMIKAZEDALIS_2.clicked.connect(self.harita_secimi_buton)
+        # BUTONLAR
+
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"NOKTA 1", None))
@@ -89,13 +106,24 @@ class Ui_MainWindow(object):
         self.baglanti.setText(QCoreApplication.translate("MainWindow", u"IHA'YA BAGLAN", None))
         self.iha_durum_2.setText(QCoreApplication.translate("MainWindow", u"tcp:127.0.0.1:5762", None))
         self.goruntu.setText(QCoreApplication.translate("MainWindow", u"DODO", None))
-    # retranslateUi
+        self.goruntu_2.setText(QCoreApplication.translate("MainWindow", u"DODO", None))
+        self.KAMIKAZEDALIS_2.setText(QCoreApplication.translate("MainWindow", u"Harita Secimi Yap", None))
+        self.comboBox_2.setItemText(0, QCoreApplication.translate("MainWindow", u"HEDEF IHA HARITA", None))
+        self.comboBox_2.setItemText(1, QCoreApplication.translate("MainWindow", u"KAMIKAZE HARITA", None))
 
+        self.comboBox_2.setCurrentText(QCoreApplication.translate("MainWindow", u"HEDEF IHA HARITA", None))
+    # retranslateUi
         self.baglanti_sayac = 0
         self.hedef_lat = [40.233100, 40.231331, 40.229251, 40.231986]
         self.hedef_lon = [29.001739, 29.007768, 29.009313, 29.010343]
 
-    def iha_baglan(self):
+    def harita_secimi_buton(self):
+        if (self.comboBox_2.currentText() == "HEDEF IHA HARITA"):
+            self.harita = 0
+        else : self.harita =1
+
+
+    def iha_baglan_buton(self):
         if len(self.iha_durum_2.text()) < 1:
             msg = QMessageBox()
             msg.setWindowTitle("BAGLANTI UYARISI")
@@ -114,7 +142,7 @@ class Ui_MainWindow(object):
             x = msg.exec_()
             self.iha_durum.setText(str(self.vehicle.mode))
 
-    def kamikaze_hazirlik(self):
+    def kamikaze_hazirlik_buton(self):
         if self.baglanti_sayac > 0:
             sayac = int(self.comboBox.currentText()[5:])
             print(self.hedef_lat[sayac - 1], self.hedef_lon[sayac - 1])
@@ -133,7 +161,6 @@ class Ui_MainWindow(object):
             self.vehicle.mode = "FBWA"
             self.vehicle.mode = "AUTO"
 
-
     def setup_camera(self):
         """Initialize camera.
         """
@@ -141,12 +168,27 @@ class Ui_MainWindow(object):
 
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.video_size.width())
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.video_size.height())
-        self.ihalar = [5, 6, 7, 8, 9, 10, 11, 12, 13]
-        #self.iha_lat, self.iha_lon = 40.15124, 29.12142
+
         self.timer = QTimer()
 
         self.timer.timeout.connect(self.display_video_stream)
         self.timer.start(30)
+    def hedef_gosterme_hazirlik(self):
+        self.timer2 = QTimer()
+        self.timer2.timeout.connect(self.display_harita)
+        self.timer2.start(30)
+
+    def display_harita(self):
+        frame = hedef_harita(self.ihalar,self.lat,self.lon)
+        self.iha_lat , self.iha_lon = self.iha_koordinat_al()
+        frame2 = kamikaze_harita(self.iha_lat,self.iha_lon)
+        if self.harita == 0:
+            frame = frame
+        else : frame = frame2
+        frame = cv2.resize(frame,(700,550))
+        image = QImage(frame, frame.shape[1], frame.shape[0],
+                       frame.strides[0], QImage.Format_RGB888)
+        self.goruntu_2.setPixmap(QPixmap.fromImage(image))
     def display_video_stream(self):
         """Read frame from camera and repaint QLabel widget.
         """
@@ -154,21 +196,11 @@ class Ui_MainWindow(object):
         self.lat[0] = self.iha_lat
         self.lon[0] = self.iha_lon
         _, frame = self.capture.read()
-        frame = cv2.resize(frame,(600,550))
-        frame2 = kamikaze_harita(self.iha_lat,self.iha_lon)
-        frame2 = cv2.resize(frame2,(600,550))
-        frame3 = hedef_harita(self.ihalar,self.lat,self.lon)
-        frame3 = cv2.resize(frame3,(600,550))
-
         frame = cv2.cvtColor(frame,  cv2.COLOR_BGR2RGB)
         frame = cv2.flip(frame, 1)
-
-        frame = np.hstack((frame2, frame))
-        frame = np.hstack((frame,frame3))
         image = QImage(frame, frame.shape[1], frame.shape[0],
                        frame.strides[0], QImage.Format_RGB888)
         self.goruntu.setPixmap(QPixmap.fromImage(image))
-
     def iha_koordinat_al(self):
         if self.baglanti_sayac > 0:
             return self.vehicle.location.global_relative_frame.lat, self.vehicle.location.global_relative_frame.lon
@@ -260,6 +292,7 @@ def lat_lon_to_pixel(lat, lon):
     sonuc_piksel_y = sonuc_lat / latlar_fark * 350
 
     return int(sonuc_piksel_x), int(sonuc_piksel_y)
+
 
 
 if __name__ == "__main__":
